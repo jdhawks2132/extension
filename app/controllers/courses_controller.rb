487a991b66ba7accfe1919e3071ce8238838140c
Rodrigo Alba <rodrigo@megabadvisors.com>
2022-01-04 12:03:25 -0400

    routes

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,2 +1,20 @@
 class CoursesController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+    def index
+        courses = Course.all
+        render json: courses
+    end
+
+    def show
+        course = Course.find(params[:id])
+        render json: course
+    end
+    
+    private
+
+    def record_not_found
+        render json: { error: "Course not found" }, status: :not_found
+    end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
   
-  resources :courses
+  resources :courses, only: [:index, :show]
   resources :reviews
-  resources :users
+
+  post "/signup", to: "users#create"
+  get "/me", to: "users#show"
+
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
+
+#index and show for courses
