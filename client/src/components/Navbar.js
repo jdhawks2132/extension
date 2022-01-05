4ed98eb72e3ee27e214bd589e73a40f28848772c
Joshua <jdhawks@gmail.com>
2022-01-05 10:38:26 -0600

    auth and seeds

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,32 +1,42 @@
 import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
 // styles & images
 import './Navbar.css';
 
 export default function Navbar({ setCurrentUser, currentUser }) {
+	const history = useHistory();
 	const handleLogout = () => {
 		setCurrentUser(null);
 		fetch('api/logout', { method: 'DELETE' });
+		history.push('/login');
 	};
+
 	return (
 		<nav className='navbar'>
 			<ul>
 				<li className='logo'>
 					<h1>//Extension</h1>
 				</li>
-				<>
-					<li>
-						<Link to='/login'>Login</Link>
-					</li>
-					<li>
-						<Link to='/signup'>Signup</Link>
-					</li>
+
+				{!currentUser && (
+					<>
+						<li>
+							<Link to='/login'>Login</Link>
+						</li>
+						<li>
+							<Link to='/signup'>Signup</Link>
+						</li>
+					</>
+				)}
+
+				{currentUser && (
 					<li>
 						<button className='btn' onClick={handleLogout}>
 							Logout
 						</button>
 					</li>
-				</>
+				)}
 			</ul>
 		</nav>
 	);
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,32 @@
-puts "Seeding courses..."
+puts 'Seeding courses...'
 
-Course.create([
-    {}
-])
-puts "✅ Done seeding courses!"
+Course.create(
+	name: 'Ruby on Rails',
+	platform: 'Web Development',
+	description: 'Learn how to build web applications with Ruby on Rails',
+)
 
+Course.create(
+	name: 'JavaScript',
+	platform: 'Web Development',
+	description: 'Learn how to build web applications with JavaScript',
+)
+
+Course.create(
+	name: 'PostgreSQL',
+	platform: 'Databases',
+	description: 'Learn how to construct and query a PostgreSQL database',
+)
+
+Course.create(
+	name: 'Python',
+	platform: 'Data Science',
+	description: 'Learn how to build data science applications with Python',
+)
+
+Course.create(
+	name: 'COBOL',
+	platform: 'Data Science',
+	decscription: 'Learn how to build data science applications with COBOL',
+)
+puts '✅ Done seeding courses!'
